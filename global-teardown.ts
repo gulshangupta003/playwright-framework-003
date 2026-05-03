b6bbcf2f2221faec748764fd3b6052bb1e372a8c
gulshangupta003 <gulshangupta003@users.noreply.github.com>
2026-05-03 12:40:55 +0000

    deploy: 02a4528009772ff94312a26721ba3d438ad2e8e8

--- global-teardown.ts
+++
@@ -1,7 +0,0 @@
-import { FullConfig } from "@playwright/test";
-
-async function globalTeardown(config: FullConfig) {
-    console.log("🧹 Running global teardown...");
-};
-
-export default globalTeardown;
